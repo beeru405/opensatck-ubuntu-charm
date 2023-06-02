88458ddbb9039afd527e7bbf24b2c53ec656239a
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-06-02 12:19:51 +0530

    Update OpenInstallation.sh

--- OpenInstallation.sh
+++ OpenInstallation.sh
@@ -6,10 +6,10 @@ juju config keystone admin-password=idrbt
 juju deploy --channel zed/stable cinder
 juju deploy --channel zed/stable glance
 #juju deploy cs:neutron-api
-juju deploy --channel zed/stable rabbitmq-server
+#juju deploy --channel zed/stable rabbitmq-server
 juju deploy --channel zed/stable nova-cloud-controller
-#juju deploy --channel zed/stable percona-cluster
-juju deploy --channel 8.0/stable mysql-innodb-cluster
+#juju deploy --channel stable percona-cluster
+#juju deploy --channel 8.0/stable mysql-innodb-cluster
 juju deploy --channel 3.9/stable rabbitmq-server
 #juju deploy cs:neutron-gateway
-juju deploy --channel 8.0/stable mysql-router
+#juju deploy --channel 8.0/stable mysql-router
